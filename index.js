1e7b3e71b3d6790a4d59dc45f46a0eb59dbeee06
Steven Doran <steven.doran@flatironschool.com>
2019-12-09 16:43:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  let nameList = []
+
+  for (const driver of drivers) {
+    if (driver.toLowerCase() === name.toLowerCase() ) {
+      nameList.push(driver)
+    }
+  }
+
+  return nameList
+}
+
+function fuzzyMatch(drivers, subString) {
+  let nameList = []
+
+  for(const driver of drivers) {
+    if (driver.substring(0,subString.length) === subString) {
+      nameList.push(driver)
+    }
+  }
+
+  return nameList
+}
+
+function matchName(drivers, name) {
+  let nameList = []
+
+  for(const driver of drivers) {
+    if (driver.name === name) {
+      nameList.push(driver)
+    }
+  }
+
+  return nameList
+}
